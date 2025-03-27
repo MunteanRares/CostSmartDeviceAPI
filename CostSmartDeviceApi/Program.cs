2b98009241ed73930f8d02db8fb40701b6a3285a
ByatisGlaaki <rares_famuntean@yahoo.com>
2025-03-27 14:27:48 +0200

    Added Cors

--- CostSmartDeviceApi/Program.cs
+++ CostSmartDeviceApi/Program.cs
@@ -13,11 +13,26 @@ builder.Services.AddSwaggerGen();
 
 // DSEPENDENCY INJECTION
 builder.Services.AddTransient<IDevicesService, DevicesService>();
-    
+
+
+//Add CORS  
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");    
 
 app.UseAuthorization();
 
